d19c488ca0c531a6b244afc0a3b27fab73de9e4b
Christian <chri.pae@gmail.com>
2019-05-15 22:39:29 +0200

    Other changes

--- test/test.js
+++ test/test.js
@@ -8,3 +8,4 @@ require('./eventHandlerManager.test');
 require('./query.test');
 require('./integration.test');
 require('./contract-testing');
+console.log(process.env.ENV_VAR, process.env.ENV_VAR2);
