c87f575ea4dfba6b6a546f0d823660493dc74487
Christian Paesante <christian.paesante@tesisquare.com>
2019-05-30 12:50:53 +0200

    Test files reorganization
    - Moved denorm contract test files under ./test/contract-test/consumerTest
    - Moved mongodb denorm unit tests under ./test/denormalizer/mongodb

--- test/denormalizer/mongodb/contract-test/index.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const Event = require('@danver97/event-sourcing/event');
-const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
-const MongoClient = require('mongodb').MongoClient;
-const orderControl = require('../../../../infrastructure/denormalizers/mongodb/orderControl')('testdb');
-const writerFunc = require('../../../../infrastructure/denormalizers/mongodb/writer');
-const handlerFunc = require('../../../../infrastructure/denormalizers/mongodb/handler');
-// const utils = require('./utils');
