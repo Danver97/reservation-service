3cba79b33388fc6e9bec7781c1c97cea53df558e
Christian Paesante <christian.paesante@tesisquare.com>
2019-05-30 12:19:45 +0200

    Moved mongodb denormalizer test files
    Moved test files from ./test/denormalizer to ./test/denormalizer/mongodb

--- test/denormalizer/mongodb/unit-test/handler.test.js
+++ test/denormalizer/mongodb/unit-test/handler.test.js
@@ -2,9 +2,9 @@ const assert = require('assert');
 const Event = require('@danver97/event-sourcing/event');
 const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
 const MongoClient = require('mongodb').MongoClient;
-const orderControl = require('../../../infrastructure/denormalizers/mongodb/orderControl')('testdb');
-const writerFunc = require('../../../infrastructure/denormalizers/mongodb/writer');
-const handlerFunc = require('../../../infrastructure/denormalizers/mongodb/handler');
+const orderControl = require('../../../../infrastructure/denormalizers/mongodb/orderControl')('testdb');
+const writerFunc = require('../../../../infrastructure/denormalizers/mongodb/writer');
+const handlerFunc = require('../../../../infrastructure/denormalizers/mongodb/handler');
 const utils = require('./utils');
 
 const mongod = new MongoMemoryServer();
