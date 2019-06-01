05e030b4590ec5db5bdf0ab146606f4f89f32ec5
Christian Paesante <christian.paesante@tesisquare.com>
2019-06-01 18:30:16 +0200

    Fixed writer.test unit test

--- test/denormalizer/mongodb/utils.js
+++ test/denormalizer/mongodb/utils.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v4');
+const getTables = require('../../test-utils').getTables;
 
 const defaultRestId = '52db11c3-ffed-489e-984b-ef2972f56d8f';
 const defaultUserId = 'c398ffa9-4cdd-4068-b3af-519b3b2e95b7';
@@ -24,19 +25,30 @@ const timeTable = {
     Sunday: tipicalDay,
 };
 
+function tables(restId) {
+    return getTables(defaultRestId || restId).map(t => {
+        const newT = JSON.parse(t.toString());
+        delete newT.reservations;
+        return newT;
+    });
+}
+
 function restaurantReservations(streamId) {
     return {
         _id: defaultRestId || streamId,
         restId: defaultRestId || streamId,
         timeTable: timeTable,
-        tables: [],
+        tables: tables(streamId),
+        reservations: [],
         _revisionId: 1,
     };
 }
 
 function reservation(restId, userId) {
+    const resId = uuid();
     return {
-        id: uuid(),
+        _id: resId,
+        id: resId,
         restId: restId || defaultRestId,
         status: 'pending',
         statusCode: 0,
@@ -44,11 +56,22 @@ function reservation(restId, userId) {
         reservationName: 'Lucio',
         people: 4,
         date: new Date(),
+        _revisionId: 1,
     };
+}
+
+function reservationToAdd(restId) {
+    return {
+        resId: uuid(),
+        restId: restId || defaultRestId,
+        table: tables(restId)[0],
+        date: new Date(),
+    }
 }
 
 module.exports = {
     timeTable,
     restaurantReservations,
     reservation,
+    reservationToAdd,
 };
--- test/denormalizer/mongodb/writer.test.js
+++ test/denormalizer/mongodb/writer.test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const uuid = require('uuid/v4');
 const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
 const MongoClient = require('mongodb').MongoClient;
 const Event = require('@danver97/event-sourcing/event');
@@ -11,8 +12,18 @@ let collection = null;
 let writer = null;
 
 describe('writer unit test', function () {
-    const rr = utils.restaurantReservations();
-    const reservation = utils.reservation();
+    /*const restId = uuid();
+    const resId = uuid();
+    const userId = uuid();
+    const reservationName = 'Smith family'
+    const people = 6;
+    const date = new Date();
+    date.setHours(date.getHours + 24);
+    let rr;
+    let r;*/
+    let rr = utils.restaurantReservations();
+    let res = utils.reservation();
+    let rToAdd = utils.reservationToAdd(rr.restId);
 
     before(async function () {
         this.timeout(10000);
@@ -27,6 +38,13 @@ describe('writer unit test', function () {
         writer = await writerFunc(mongoConfig);
     });
 
+    beforeEach(() => {
+        rr = utils.restaurantReservations();
+        res = utils.reservation();
+        rToAdd = utils.reservationToAdd(rr.restId);
+        return collection.deleteMany({});
+    });
+
     it('check if restarantReservationCreated works', async function () {
         const e = new Event(rr.restId, 1, 'restaurantCreated', rr);
         await writer.restaurantReservationsCreated(e.payload);
@@ -35,58 +53,94 @@ describe('writer unit test', function () {
     });
 
     it('check if reservationAdded works', async function () {
-        const res = { id: reservation.id, date: reservation.date, userId: reservation.userId, people: reservation.people };
-        res.table = { id: 15, people: 4 };
-        rr.reservations.push(res);
+        // Preset
+        collection.insertOne(rr);
+        
+        // Update to do
+        rr.reservations.push(rToAdd);
         rr._revisionId++;
-        const e = new Event(rr.restId, 2, 'reservationAdded', res);
-        await writer.reservationAdded(rr.restId, e.eventId - 1, res);
+
+        const e = new Event(rr.restId, 2, 'reservationAdded', rToAdd);
+        await writer.reservationAdded(rr.restId, e.eventId - 1, rToAdd);
         const doc = await collection.findOne({ _id: rr.restId });
         assert.deepStrictEqual(doc, rr);
     });
 
     it('check if reservationRemoved works', async function () {
-        rr.reservations = rr.reservations.filter(r => r.id != reservation.id);
+        // Preset
+        rr.reservations.push(rToAdd);
         rr._revisionId++;
-        const e = new Event(rr.restId, 3, 'reservationRemoved', { restId: rr.restId, resId: reservation.id });
+        const newDoc = Object.assign({ _id: rr.restId, _revisionId: 2 }, rr);
+        collection.insertOne(newDoc);
+        
+        // Update to do
+        rr.reservations = rr.reservations.filter(r => r.id != rToAdd.resId);
+        rr._revisionId++;
+
+        const e = new Event(rr.restId, 3, 'reservationRemoved', { restId: rr.restId, resId: res.id });
         await writer.reservationRemoved(rr.restId, e.eventId - 1, e.payload.resId);
         const doc = await collection.findOne({ _id: rr.restId });
         assert.deepStrictEqual(doc, rr);
     });
 
     it('check if reservationCreated works', async function () {
-        const e = new Event(rr.restId, 1, 'reservationCreated', reservation);
-        await writer.reservationCreated(reservation);
-        const doc = await collection.findOne({ _id: reservation.id });
-        assert.deepStrictEqual(doc, reservation);
+        // Preset
+        res.resId = res.id;
+        delete res.id;
+
+        const e = new Event(rr.restId, 1, 'reservationCreated', res);
+        await writer.reservationCreated(res);
+        const doc = await collection.findOne({ _id: res.resId });
+        assert.deepStrictEqual(doc, res);
     });
 
     it('check if reservationConfirmed works', async function () {
-        reservation.table = { id: 15, people: 4 };
-        reservation.status = 'confirmed';
-        reservation._revisionId++;
-        const e = new Event(rr.restId, 2, 'reservationConfirmed', { table: reservation.table, status: reservation.status });
-        await writer.reservationConfirmed(reservation.id, e.eventId - 1, e.payload);
-        const doc = await collection.findOne({ _id: reservation.id });
-        assert.deepStrictEqual(doc, reservation);
+        // Preset
+        const newDoc = Object.assign({ _id: res.id, _revisionId: 1 }, res);
+        collection.insertOne(newDoc);
+        
+        // Update to do
+        res.table = { id: 15, people: 4 };
+        res.status = 'confirmed';
+        res._revisionId++;
+
+        const e = new Event(rr.restId, 2, 'reservationConfirmed', { table: res.table, status: res.status });
+        await writer.reservationConfirmed(res.id, e.eventId - 1, e.payload);
+        const doc = await collection.findOne({ _id: res.id });
+        assert.deepStrictEqual(doc, res);
     });
 
     it('check if reservationRejected works', async function () {
-        reservation.status = 'rejected';
-        reservation._revisionId++;
-        const e = new Event(rr.restId, 3, 'reservationRejected', { status: reservation.status });
-        await writer.reservationRejected(reservation.id, e.eventId - 1, reservation.status);
-        const doc = await collection.findOne({ _id: reservation.id });
-        assert.deepStrictEqual(doc, reservation);
+        // Preset
+        const newDoc = Object.assign({ _id: res.id, _revisionId: 1 }, res);
+        collection.insertOne(newDoc);
+
+        // Update to do
+        res.status = 'rejected';
+        res._revisionId++;
+
+        const e = new Event(rr.restId, 2, 'reservationRejected', { status: res.status });
+        await writer.reservationRejected(res.id, e.eventId - 1, res.status);
+        const doc = await collection.findOne({ _id: res.id });
+        assert.deepStrictEqual(doc, res);
     });
 
     it('check if reservationCancelled works', async function () {
-        reservation.status = 'cancelled';
-        reservation._revisionId++;
-        const e = new Event(rr.restId, 4, 'reservationCancelled', { status: reservation.status });
-        await writer.reservationCancelled(reservation.id, e.eventId - 1, reservation.status);
-        const doc = await collection.findOne({ _id: reservation.id });
-        assert.deepStrictEqual(doc, reservation);
+        // Preset
+        res.table = { id: 15, people: 4 };
+        res.status = 'confirmed';
+        res._revisionId++;
+        const newDoc = Object.assign({ _id: res.id, _revisionId: 2 }, res);
+        collection.insertOne(newDoc);
+
+        // Update to do
+        res.status = 'cancelled';
+        res._revisionId++;
+
+        const e = new Event(rr.restId, 3, 'reservationCancelled', { status: res.status });
+        await writer.reservationCancelled(res.id, e.eventId - 1, res.status);
+        const doc = await collection.findOne({ _id: res.id });
+        assert.deepStrictEqual(doc, res);
     });
 
     after(async () => {
