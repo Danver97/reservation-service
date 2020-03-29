c75dbe0d71ab21c4b9e2067ccfb6505b9f8e28c7
Danver97 <chri.pae@gmail.com>
2020-03-29 19:08:11 +0200

    No more needed tests

--- test/unit.test.js
+++ test/unit.test.js
@@ -3,7 +3,7 @@ require('./unit-test/table.test');
 require('./unit-test/restaurantReservations.test');
 require('./unit-test/repositoryManager.test');
 require('./unit-test/restaurantReservationsManager.test');
-require('./unit-test/eventHandlerManager.test');
+// require('./unit-test/eventHandlerManager.test');
 require('./unit-test/query.test');
 require('./unit-test/denormalizers');
 require('./unit-test/api.test');
